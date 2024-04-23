74984deaf994109a9bd56cbb8ef8f9776193716d
ChristineCRATO <crato.christine@orange.fr>
2024-04-23 20:55:54 +0200

    add arrow left & right & listener/click

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,29 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Déiflement de l'Index à l'infini
+currentSlideIndex = 0
+console.log(click.length)
+
+// Arrow Left : sélection 👈 dans HTML et ajout "click" 
+let arrowLeft = document.querySelector(".arrow_left")
+arrowLeft.addEventListener("click", clickLeft)
+
+// Fonction défilement click Left 
+function clickLeft() {
+	currentSlideIndex = (currentSlideIndex - 1 + clicks.length) % clicks.length
+	console.log("click arrow Left")
+	updateClick(currentSlideIndex -1)
+}
+
+// Arrow Right : sélection 👉 dans HTML et ajout "click"
+let arrowRight = document.querySelector(".arrow_right")
+arrowRight.addEventListener("click", clickRight)
+
+// Fonction défilement click Right
+function clickRight() {
+	currentSlideIndex = (currentSlideIndex + 1) % click.length
+	console.log("click arrow Right")
+	updateClick(currentSlideIndex + 1)
+}
